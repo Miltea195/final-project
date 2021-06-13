2c66ba3d5593f5f6f29b1e4b55d4255a0722315c
Denis <desemmobil@gmail.com>
2021-06-13 10:17:39 +0300

    final

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         waitForAnimate: false,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        // pauseOnFocus: true,
-        // pauseOnHover: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
     });
 })
 $(document).ready(function(){
